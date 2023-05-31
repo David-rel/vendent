330748a7f165f0446378d286f9a5b87acecb9bb4
David Fales <davidfalesct@gmail.com>
2023-05-31 10:57:19 -0600

    added auth check 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { AuthProvider } from "@propelauth/react";
+import React from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider authUrl={process.env.NEXT_PUBLIC_AUTH_URL}>
+      <Head>
+        <title>Vendent</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="./static/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
+import { useRedirectFunctions, withAuthInfo, useLogoutFunction } from "@propelauth/react";
 
-export default function Home() {
+const Home = withAuthInfo((props) => {
+  const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } =
+    useRedirectFunctions();
+  const logoutFunction = useLogoutFunction();
 
+  // isLoggedIn and user are injected automatically from withAuthInfo
+  if (props.isLoggedIn) {
+    return (
+      <div>
+        <p>You are logged in as {props.user.email}</p>
+        <button onClick={redirectToAccountPage}>Account</button>
+        <button onClick={logoutFunction}>Logout</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>You are not logged in</p>
+        <button onClick={redirectToLoginPage}>Login</button>
+        <button onClick={redirectToSignupPage}>Signup</button>
+      </div>
+    );
+  }
+});
 
-
-  return (
-    <></>
-  )
-}
+export default Home;
